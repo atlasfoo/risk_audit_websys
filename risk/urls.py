762c72c961717d546a70bc8fd71d6296b3ee52be
atlasfoo@lin <jf_mejiar547@live.com>
2021-05-29 22:23:25 -0600

    :construction: - finished risk model list and detail view

--- risk/urls.py
+++ risk/urls.py
@@ -4,5 +4,5 @@ from risk.views import RiskListView, RiskDetailView
 
 risk_urls = ([
     path('', RiskListView.as_view(), name='list'),
-    path('<int:pk>', RiskDetailView.as_view(), name='list'),
+    path('<int:pk>', RiskDetailView.as_view(), name='risk'),
 ], 'risks')
