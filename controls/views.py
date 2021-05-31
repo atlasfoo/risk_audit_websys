c4815987e2d6229041294872240171001a2a6672
atlasfoo@lin <jf_mejiar547@live.com>
2021-05-30 23:03:29 -0600

    :lock::construction::lipstick: - app views auth lock, minor style reconfigurations

--- controls/views.py
+++ controls/views.py
@@ -1,5 +1,7 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from controls.forms import ControlForm
@@ -14,12 +16,14 @@ class ControlDetailView(DetailView):
     model = Control
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class CreateControlView(CreateView):
     model = Control
     success_url = reverse_lazy('controls:list')
     form_class = ControlForm
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class UpdateControlView(UpdateView):
     model = Control
     template_name_suffix = "_update_form"
--- risk/views.py
+++ risk/views.py
@@ -1,4 +1,6 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from risk.forms import RiskForm, CauseForm, EffectForm
@@ -14,12 +16,14 @@ class RiskDetailView(DetailView):
     model = Risk
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class CreateRiskView(CreateView):
     model = Risk
     success_url = reverse_lazy('risks:list')
     form_class = RiskForm
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class UpdateRiskView(UpdateView):
     model = Risk
     template_name_suffix = "_update_form"
@@ -38,12 +42,14 @@ class CauseDetailView(DetailView):
     model = Cause
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class CreateCauseView(CreateView):
     model = Cause
     success_url = reverse_lazy('risks:cause_list')
     form_class = CauseForm
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class UpdateCauseView(UpdateView):
     model = Cause
     template_name_suffix = "_update_form"
@@ -62,12 +68,14 @@ class EffectDetailView(DetailView):
     model = Effect
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class CreateEffectView(CreateView):
     model = Effect
     success_url = reverse_lazy('risks:effect_list')
     form_class = EffectForm
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class UpdateEffectView(UpdateView):
     model = Effect
     template_name_suffix = "_update_form"
